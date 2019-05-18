ced698c4170843f48c9022c97d75a25be10c8dca
Becki Blumer <rblumer13@gmail.com>
2019-05-18 12:19:38 -0500

    added pseudocode to index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+function getTopTracks() {
+    // Use Spotify API to get top tracks from selected artist for listening in web player
+    // RESEARCH WEB PLAYER IMPLEMENTATION
+}
+
+function watchArtists() {
+    // Listen for user to click an artist option then run getTopTracks
+}
+
+function getMoreEvents() {
+    // Listen for the user to click the "see more events" button then run getEvents/renderEventList again for more items
+    // NOT A PRIORITY
+}
+
+function renderEventList() {
+    // Use the data from the getEvents function to render the Event list in the browser
+}
+
+function getEvents() {
+    // Use the location ID from user location selection to fetch event data for their indicated dates
+}
+
+function watchLocations() {
+    // Listen for user to click a location option (event delegation) and get the location ID from user selection
+}
+
+function renderDates() {
+    // Render the dates the user has submitted in the browser heading
+}
+
+function renderLocations() {
+    // Use data from Songkick to render a list of location options for user to select
+}
+
+function getLocations() {
+    // Use Songkick API to fetch location options
+}
+
+function watchForm() {
+    // Listen for user to input location and dates and click submit
+};
+
+watchForm();
