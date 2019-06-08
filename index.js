bde02cbd37e1c8f3d04e0dc3aae065662e465a75
Becki Blumer <rblumer13@gmail.com>
2019-06-08 08:18:15 -0500

    updated loader styles

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ function playNapster(responseJson, apikeyN) {
                 document.getElementById('tracks-container').innerHTML =
                     `<p class="error">There are no tracks available for this artist.</p>`
             }
-            else {$tracks.html(tracksTemplate(response));}
-        });
-}
+            else {
+                $tracks.html(tracksTemplate(response));
+            };
+        })
+};
 
 function getArtistObject(artist) {
     console.log('The getArtistObject function ran.');
-    // 1 - Do a search query with Napster for the artist
     const apikeyN = 'MTE5OWJjOWQtOWQ5My00MmRjLWIyNmQtODkzMWY0ZjQxOTVl';
     const params = {
         apikey: apikeyN,
@@ -62,15 +63,15 @@ function getArtistObject(artist) {
 }
 
 function scrollToPlayer() {
-    let current = window.location.href;
+    const current = window.location.href;
     window.location.href = current.replace(/#(.*)$/, '') + '#musicplayer';
 }
 
 function watchArtists() {
-    // Listen for user to click an artist option then run getTopTracks
     console.log('The watchArtists function ran.');
     $('#events').on('click', `.listen`, event => {
         event.preventDefault();
+        $('#tracks-container').empty();
         const artist = event.currentTarget.innerText;
         $('.play-artist').text(artist);
         getArtistObject(artist);
@@ -78,8 +79,6 @@ function watchArtists() {
 }
 
 function getMoreEvents(params, locationDisplayName) {
-    // Listen for the user to click the "see more events" button then run getEvents/renderEventList again for more items
-    // NOT A PRIORITY
     console.log('The getMoreEvents function ran.');
     $('.see-more').click(function() {
         event.preventDefault();
@@ -118,10 +117,8 @@ function renderEventList(responseJson, locationDisplayName, params) {
     element.classList.remove('hidden');
     $('.artist-response').addClass('animated animatedFadeInUp fadeInUp');
 
-    //Update the location displayed at the top of this section
     $('.location-submitted').text(`${locationDisplayName}`);
 
-    //Use the data from the getEvents function to render the Event list in the browser
     console.log('The renderEventList function ran with response');
     console.log(responseJson);
 
@@ -131,8 +128,6 @@ function renderEventList(responseJson, locationDisplayName, params) {
 
     for (let i = 0; i < responseJson.resultsPage.results.event.length; i++){
         let concertName = `${responseJson.resultsPage.results.event[i].displayName}`;
-        let venue = `${responseJson.resultsPage.results.event[i].venue.displayName}`;
-        let date = `${responseJson.resultsPage.results.event[i].start.date}`;
         let eventLink = `${responseJson.resultsPage.results.event[i].uri}`;
 
          $('#events').append(
@@ -159,7 +154,6 @@ function renderEventList(responseJson, locationDisplayName, params) {
 }
 
 function getEvents(id, locationDisplayName, dates) {
-    // Use the location ID from user location selection to fetch event data for their indicated dates
     console.log(`The getEvents function ran with location ID ${id}.`);
     const date_min = dates[0];
     const date_max = dates[1];
@@ -203,7 +197,6 @@ function getItemIdFromElement(item) {
 }
 
 function watchLocations(dates) {
-    // Listen for user to click a location option (event delegation) and get the location ID from user selection
     console.log('The wachLocations function ran.');
     $('#locations').on('click', `.location-result`, event => {
         event.preventDefault();
@@ -217,7 +210,6 @@ function watchLocations(dates) {
 }
 
 function renderDates(dates, dateArray) {
-    // Render the dates the user has submitted in the browser heading
     console.log(`The renderDates function ran with dates = ${dates} and dateArray is`);
     console.log(dateArray);
 
@@ -240,10 +232,6 @@ function renderDates(dates, dateArray) {
     const startMonth = monthsArray[parseInt(dateArray[0].month,10)];
     const endMonth = monthsArray[parseInt(dateArray[1].month,10)];
 
-    // console.log(`The first month in the first object in dateArray is ${monthsArray[startMonth]}`);
-    // console.log(`The end month in the first object in dateArray is ${monthsArray[endMonth]}`);
-
-
     $('.date-submitted').text(`${startMonth} ${dateArray[0].day}, ${dateArray[0].year} to ${endMonth} ${dateArray[1].day}, ${dateArray[1].year}`);
 }
 
@@ -253,7 +241,6 @@ function renderLocations(responseJson, location, dates, dateArray) {
 
     renderDates(dates, dateArray);
 
-    // Use data from Songkick to render a list of location options for user to select
     console.log(`The renderLocations function ran with location ${location}, dates ${dates}, and responseJson was:`);
     console.log(responseJson)
 
@@ -274,7 +261,6 @@ function formatQueryParams(params) {
 }
 
 function getLocations(location, dates, dateArray) {
-    // Use Songkick API to fetch location options
     console.log(`The getLocations function ran with location = ${location} and dates = 
     ${dates}`);
 
@@ -288,6 +274,8 @@ function getLocations(location, dates, dateArray) {
     const queryString = formatQueryParams(params);
     const url = 'https://api.songkick.com/api/3.0/search/locations.json?' + queryString;
     console.log(`fetching data from URL ${url}`);
+
+    $('.loader').toggleClass('hidden');
 
     fetch(url)
         .then(response => {
@@ -307,7 +295,8 @@ function getLocations(location, dates, dateArray) {
         .then(responseJson => renderLocations(responseJson, location, dates, dateArray))
         .catch(err => {
             $('.js-error-message').text(`Uh oh! Something went wrong. Here's what we know: ${err.message}`);
-        });
+        })
+        .finally(() => $('.loader').toggleClass('hidden'));
 }
 
 function renderAlbumArt(oneAlbumURL) {
@@ -316,8 +305,6 @@ function renderAlbumArt(oneAlbumURL) {
     );
 
     $('#album-container').addClass('load');
-    // let myElement = document.querySelector("body");
-    // myElement.style.background="purple";
 }
 
 function getOneAlbum(responseJson) {
@@ -343,7 +330,7 @@ function getAlbumArt(responseJson) {
             .catch(err => {
                 $('.js-error-message').text(`
                 Uh oh! Something went wrong. Here's what we know: ${err.message}`)
-            })
+            });
     }
 }
 
@@ -385,56 +372,48 @@ function updateFormValues() {
         let str = document.getElementById('day1').value;
         console.log(`Value of string is ${str}`);
         document.getElementById('day1').value = str.padStart(2, "0");
-    })
+    });
 
     $('#day2').on('blur', function() {
         let str = document.getElementById('day2').value;
         console.log(`Value of string is ${str}`);
         document.getElementById('day2').value = str.padStart(2, "0");
-    })
+    });
 
     $('#year1').on('blur', function() {
         let str = document.getElementById('year1').value;
         console.log(`Value of string is ${str}`);
         document.getElementById('year1').value = str.padStart(4, "20");
-    })
+    });
 
     $('#year2').on('blur', function() {
         let str = document.getElementById('year2').value;
         console.log(`Value of string is ${str}`);
         document.getElementById('year2').value = str.padStart(4, "20");
-    })
+    });
 }
 
 function watchForm() {
-    // Listen for user to input location and dates and click submit
     console.log('The watchForm function ran.');
-
-    // window.addEventListener('DOMContentLoaded', (event) => {
-    //     console.log('DOM fully loaded and parsed');
-    //     $('#album-container').toggleClass('hidden');
-    // });
 
     updateFormValues();
 
     $('.location-submit').click(function() {
         event.preventDefault();
 
+        // Songkick date format must be YYYY-MM-DD
         let date1string = `${$('#year1').val()}-${$('#month1').val()}-${$('#day1').val()}`;
         let date1 = new Date($('#year1').val(), ($('#month1').val()-1), $('#day1').val());
 
-        // console.log(new Date(date1).toISOString().getMonth() + 1);
         const date2string = `${$('#year2').val()}-${$('#month2').val()}-${$('#day2').val()}`;
         const date2 = new Date($('#year2').val(), ($('#month2').val()-1), $('#day2').val());
 
         let today = new Date();
         let dd = today.getDate();
-        let mm = today.getMonth()+1; //As January is 0.
+        let mm = today.getMonth()+1; 
         let yyyy = today.getFullYear();
-        // const todayString = `${yyyy}-${mm}-${dd}`;
 
         let dates = [date1string,date2string];
-        //Run the getLocations function to get locations from Songkick with the form information
 
         let curdate = function(sp){            
             if(dd<10) dd='0'+dd;
@@ -497,8 +476,6 @@ function watchForm() {
         };
 
         let location = $('.location-input').val();
-        // Creating the variable 'dates' as an array with two items for min_date and max_date
-        // Songkick date format must be YYYY-MM-DD
 
         let dateArray = [
             {month:$('#month1').val(),
@@ -514,8 +491,6 @@ function watchForm() {
 
     getStaffPicks();
     getTopAlbums();
-
-    // console.log(moment.now);
 
     $('.landing-view').toggleClass('hidden');
 };
